762da360f34489769bf729f51a1d73a4b9eaf395
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-11 15:54:50 -0300

    Calcular média de 3 notas e dizer se está aprovado ou não.

---
+++ atividade14.py
@@ -0,0 +1,20 @@
+# Crie um programa que receba três notas de um aluno e calcule a média. Informe se o aluno foi aprovado, reprovado ou se está de recuperação. Use as seguintes regras:
+# Média ≥ 7: Aprovado
+# 5 ≤ Média < 7: Recuperação
+# Média < 5: Reprovado
+
+nota1 = float(input("Digite sua primeira nota: "))
+nota2 = float(input("Digite sua segunda nota: "))
+nota3 = float(input("Digite sua terceira nota: "))
+
+
+media = (nota1 + nota2 + nota3) /3
+
+
+if media >= 7:
+    print("Aprovado")
+if  media >= 5 and media <7:
+    print("Recuperação.") 
+
+if media <5:
+    print("Reprovado.")
